35227b6d66473cc73a705080269c9b0207d11ae1
FVS <felipe.ventura@bb.com.br>
2025-09-12 14:04:58 -0300

    fim aula 01-7 + hover aplicado na página

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const botaoUpload = document.getElementById("botao-upload");
+const imagemParaUpload = document.getElementById("imagem-para-upload");
+
+botaoUpload.addEventListener("click", () => {
+  imagemParaUpload.click();
+});
