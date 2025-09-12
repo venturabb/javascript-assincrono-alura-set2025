dfb848b4fad760789b807b4fa33fa37aafc98072
FVS <felipe.ventura@bb.com.br>
2025-09-12 14:15:29 -0300

    confere tipo e tamanho do arquivo via JS

--- scripts.js
+++ scripts.js
@@ -4,3 +4,15 @@ const imagemParaUpload = document.getElementById("imagem-para-upload");
 botaoUpload.addEventListener("click", () => {
   imagemParaUpload.click();
 });
+
+imagemParaUpload.addEventListener("change", (evento) => {
+  var file = evento.target.files[0];
+  if (!file.type.match("image/png") && !file.type.match("image/jpeg")) {
+    alert("Por favor, selecione uma imagem JPEG ou PNG.");
+    return;
+  }
+  if (file.size > 2 * 1024 * 1024) {
+    alert("A imagem deve ter no máximo 2 MB.");
+    return;
+  }
+});
